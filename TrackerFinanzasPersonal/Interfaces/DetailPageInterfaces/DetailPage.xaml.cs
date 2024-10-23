9b1504373b2bfb4b74ec4f58782da78084612568
Andres <andres990420@gmail.com>
2024-10-23 02:54:19 -0400

    Add project files.

---
+++ TrackerFinanzasPersonal/Interfaces/DetailPageInterfaces/DetailPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackerFinanzasPersonal.Interfaces.DetailPageInterfaces
+{
+    /// <summary>
+    /// Interaction logic for DetailPage.xaml
+    /// </summary>
+    public partial class DetailPage : UserControl
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+
+            MovimientosGastos listaGastos = new();
+
+            listaGastos.categoria = "Comida";
+            listaGastos.fecha = "10/20/2024";
+            listaGastos.monto = 50.10;
+            listaGastos.descripcion = "Cena de graduacion";
+
+            dataGridGastos.Items.Add(listaGastos);
+
+            MovimientosIngresos listaIngresos = new();
+
+            listaIngresos.fecha = listaGastos.fecha;
+            listaIngresos.categoria = "Sueldo y Salarios";
+            listaIngresos.monto = 20.22;
+            listaIngresos.descripcion = "Venta de comida";
+
+            dataGridIngresos.Items.Add(listaIngresos);
+        }
+
+        public class MovimientosGastos
+        {
+
+            public string fecha { get; set; }
+            public string categoria { get; set; }
+            public double monto { set; get; }
+            public string descripcion { set; get; }
+
+        }
+
+        public class MovimientosIngresos
+        {
+            public string fecha { get; set; }
+            public string categoria { get; set; }
+            public double monto { set; get; }
+            public string descripcion { set; get; }
+        }
+    }
+}
---
+++ TrackerFinanzasPersonal/Interfaces/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TrackerFinanzasPersonal.Interfaces;
+using TrackerFinanzasPersonal.Interfaces.DetailPageInterfaces;
+
+namespace TrackerFinanzasPersonal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DashboardPageView(object sender, RoutedEventArgs e)
+        {
+            DashboardPageButton.Visibility = Visibility.Visible;
+            DetailPagesButton.Visibility = Visibility.Hidden;
+            DashboardPage dashboard = new();
+            DashboardPageButton.Children.Add(dashboard);
+        }
+
+        private void DetailPageView(object sender, RoutedEventArgs e)
+        {
+            DashboardPageButton.Visibility = Visibility.Hidden;
+            DetailPagesButton.Visibility = Visibility.Visible;
+            DetailPage detailPage = new();
+            DetailPagesButton.Children.Add(detailPage);
+            
+        }
+
+        private void ExitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
